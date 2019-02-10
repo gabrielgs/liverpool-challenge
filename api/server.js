bfaa59b362792d3a7ad770140ceef3482b574968
Gabriel Garcia <gabrielgsdev@gmail.com>
2019-02-09 23:56:46 -0600

    Add handling errors

--- api/server.js
+++ api/server.js
@@ -15,6 +15,21 @@ app.use(logger('dev'));
 
 app.use('/api/v1/products', productRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Api is running on PORT ${port}.`);
 });
