0a3c88db53eca1dee6d1d82aecee8b9daa9d2335
Gabriel Garcia <gabrielgsdev@gmail.com>
2019-02-10 01:43:18 -0600

    Add product Schema

---
+++ api/db/models/product.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
