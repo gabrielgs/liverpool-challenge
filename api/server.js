832c6babdedb010307784d1ddfa9c42362d6a14f
Gabriel Garcia <gabrielgsdev@gmail.com>
2019-02-10 00:37:54 -0600

    Add Allow CORS

--- api/server.js
+++ api/server.js
@@ -13,6 +13,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 
+// Allow CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if(req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({})
+  }
+  next();
+});
+
+// Routes which should handle requests
 app.use('/api/v1/products', productRoutes);
 
 app.use((req, res, next) => {
