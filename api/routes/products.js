feaded6ff127537e6f7525c184f132346da9a94f
Gabriel Garcia <gabrielgsdev@gmail.com>
2019-02-10 00:17:02 -0600

    Try body-parser

--- api/routes/products.js
+++ api/routes/products.js
@@ -9,8 +9,14 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  }
+
   res.status(201).json({
-    message: 'Handling POST REQUEST TO /products'
+    message: 'Handling POST REQUEST TO /products',
+    createdProduc: product
   });
 });
 
