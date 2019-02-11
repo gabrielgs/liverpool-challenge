975b15c878c2920f5fcfd8f0a02a10d8d8db0237
Gabriel Garcia <gabrielgsdev@gmail.com>
2019-02-11 01:08:14 -0600

    Add controllers to keep the routes cleaner

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import Product from '../db/models/product'
-import multer  from 'multer';
+import express            from 'express';
+import multer             from 'multer';
+import ProductsController from '../controllers/products';
 
 const router = express.Router();
 
@@ -31,104 +31,14 @@ const upload = multer({
 });
 
 // Routes which handle requests
-router.get('/', async (req, res, next) => {
-  try {
-    const getAllProducts = await Product.find();
+router.get('/', ProductsController.products_get_all);
 
-    if (getAllProducts.length === 0)
-      return res.status(400).json({ message: 'No hay productos registrados'});
+router.post('/', upload.single('productImage'), ProductsController.products_create_product);
 
-    res.status(200).json({ products: getAllProducts });
-  } catch(err) {
-    res.status(500).json({ error: err });
-  }
-});
+router.get('/:productId', ProductsController.products_get_product);
 
-router.post('/', upload.single('productImage'), async (req, res, next) => {
-  try {
-    console.log(req.file);
-    const newProduct = {
-      name: req.body.name,
-      price: req.body.price,
-      productImage: req.file.path
-    }
+router.patch('/:productId', ProductsController.products_update_product);
 
-    const isProductExist = await Product.findOne(newProduct);
-
-    if (isProductExist)
-      return res.status(400).json({ message: 'El producto ya esta registrado'});
-
-    const product = await Product.create(newProduct);
-
-    res.status(201).json({
-      message: 'Producto creado satisfactoriamente',
-      createdProduc: product
-    });
-
-  } catch(err) {
-    res.status(500).json({ error: err})
-  }
-});
-
-router.get('/:productId', async (req, res, next) => {
-  try{
-    const { productId } = req.params;
-
-    const getProduct = await Product.findById({ _id: productId });
-
-    if (!getProduct)
-      return res.status(400).json({ message: "Producto no encontrado" });
-
-    res.status(200).json({ product: getProduct });
-  } catch(err) {
-    res.status(500).json({ error: err });
-  }
-
-});
-
-router.patch('/:productId', async (req, res, next) => {
-  try {
-    const { productId } = req.params;
-    let updateFields = {};
-
-    for (const field of req.body) {
-      updateFields[field.propName] = field.value;
-    }
-
-    const updateProduct = await Product.findOneAndUpdate(
-        { _id: productId},
-        { $set: updateFields},
-        {new: true}
-    );
-
-    if (!updateProduct)
-      return res.status(400).json({ message: 'Producto no encontrado'});
-
-    res.status(200).json({
-      message: 'Updated product!',
-      product: updateProduct
-    });
-  } catch(err) {
-    res.status(500).json({ error: 'Something Wrong happened' });
-  }
-});
-
-router.delete('/:productId', async (req, res, next) => {
-  try {
-    const { productId } = req.params
-
-    const productDeleted = await Product.findByIdAndRemove({ _id: productId });
-
-    if (!productDeleted)
-      return res.status(400).json({ message: 'Producto no encontrado'});
-
-    res.status(200).json({
-      message: 'Deleted product!',
-      product: productDeleted
-    });
-  } catch(err) {
-    res.status(500).json({ error: err });
-  }
-});
+router.delete('/:productId', ProductsController.products_delete_product);
 
 export default router;
